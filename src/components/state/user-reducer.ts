3c7dce0f757a01aabe5a270ac1362606e8da8d82
boevoivertolet <bjha78rus@yandex.ru>
2022-05-17 15:46:35 +0300

    refactor for immutability

--- src/components/state/user-reducer.ts
+++ src/components/state/user-reducer.ts
@@ -9,16 +9,18 @@ type ActionType = {
 }
 
 
-
-export const userReducer = (state: StateType, action: ActionType) => {
+export const userReducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1;
-            return state;
+            let newState ={...state}
+            newState.age = state.age + 1;
+            return newState;
         case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1;
-            return state;
+            return{
+                ...state,
+                childrenCount: state.childrenCount +1
+            }
         default:
-            throw new Error("I don't understand this type")
+            throw new Error('I don\'t understand this type')
     }
 }
